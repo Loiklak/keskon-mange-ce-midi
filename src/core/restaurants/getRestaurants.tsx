8a687b9243845e315673430fa0496c90636f0c37
Loïc Chau <chauloic77@gmail.com>
2023-09-17 23:09:18 +0200

    feat: allow random restaurant pick

---
+++ src/core/restaurants/getRestaurants.tsx
@@ -0,0 +1,92 @@
+export type Restaurant = {
+  name: string;
+  tags?: string[];
+  canTakeAway: boolean;
+  canEatIn: boolean;
+};
+
+export const getRandomRestaurant = (): Restaurant =>
+  getRestaurants()[Math.floor(Math.random() * getRestaurants().length)];
+
+export const getRestaurants = (): Restaurant[] => RESTAURANTS;
+
+const RESTAURANTS = [
+  {
+    name: "Via Roma",
+    tags: [],
+    canTakeAway: true,
+    canEatIn: false,
+  },
+  {
+    name: "Banh Mila",
+    canTakeAway: true,
+    canEatIn: false,
+    tags: ["asiatique"],
+  },
+  {
+    name: "Ichiban Sushi",
+    tags: ["asiatique"],
+    canTakeAway: true,
+    canEatIn: false,
+  },
+  {
+    name: "Y-Izakaya",
+    tags: ["asiatique"],
+    canTakeAway: false,
+    canEatIn: true,
+  },
+  {
+    name: "Krispy (KFC)",
+    tags: ["asiatique"],
+    canTakeAway: true,
+    canEatIn: true,
+  },
+  {
+    name: "Enpanadas",
+    tags: ["amerique latine"],
+    canTakeAway: true,
+    canEatIn: false,
+  },
+  {
+    name: "Barepas",
+    tags: ["amerique latine"],
+    canTakeAway: true,
+    canEatIn: true,
+  },
+  {
+    name: "Billy Brick",
+    tags: [],
+    canTakeAway: true,
+    canEatIn: false,
+  },
+  {
+    name: "Shin Jung",
+    tags: ["asiatique"],
+    canTakeAway: true,
+    canEatIn: true,
+  },
+  {
+    name: "Mme Shawn",
+    tags: ["asiatique"],
+    canTakeAway: true,
+    canEatIn: true,
+  },
+  {
+    name: "Pokeshop",
+    tags: ["asiatique"],
+    canEatIn: false,
+    canTakeAway: true,
+  },
+  {
+    name: "Parisik",
+    tags: ["asiatique"],
+    canEatIn: true,
+    canTakeAway: true,
+  },
+  {
+    name: "Augustin",
+    tags: [],
+    canEatIn: true,
+    canTakeAway: false,
+  },
+];
