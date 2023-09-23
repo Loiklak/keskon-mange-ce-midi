7744878ad694944d7a17f21eb3f13fe67ccec46f
Loïc Chau <chauloic77@gmail.com>
2023-09-23 13:53:23 +0200

    feat: get restaurants from db

--- src/core/restaurants/getRestaurants.tsx
+++ src/core/restaurants/getRestaurants.tsx
@@ -1,94 +1,20 @@
 "use server";
 
+import prisma from "../../../prisma/singleton";
+
 export type Restaurant = {
   name: string;
   tags?: string[];
-  canTakeAway: boolean;
   canEatIn: boolean;
 };
 
-export const getRandomRestaurant = async (): Promise<Restaurant> =>
-  getRestaurants()[Math.floor(Math.random() * getRestaurants().length)];
+export const getRandomRestaurant = async (): Promise<Restaurant> => {
+  const restaurantCount = await prisma.restaurant.count();
+  const skip = Math.floor(Math.random() * restaurantCount);
+  const arrayWithRandomRestaurant = await prisma.restaurant.findMany({
+    take: 1,
+    skip: skip,
+  });
 
-const getRestaurants = (): Restaurant[] => RESTAURANTS;
-
-const RESTAURANTS = [
-  {
-    name: "Via Roma",
-    tags: [],
-    canTakeAway: true,
-    canEatIn: false,
-  },
-  {
-    name: "Banh Mila",
-    canTakeAway: true,
-    canEatIn: false,
-    tags: ["asiatique"],
-  },
-  {
-    name: "Ichiban Sushi",
-    tags: ["asiatique"],
-    canTakeAway: true,
-    canEatIn: false,
-  },
-  {
-    name: "Y-Izakaya",
-    tags: ["asiatique"],
-    canTakeAway: false,
-    canEatIn: true,
-  },
-  {
-    name: "Krispy (KFC)",
-    tags: ["asiatique"],
-    canTakeAway: true,
-    canEatIn: true,
-  },
-  {
-    name: "Enpanadas",
-    tags: ["amerique latine"],
-    canTakeAway: true,
-    canEatIn: false,
-  },
-  {
-    name: "Barepas",
-    tags: ["amerique latine"],
-    canTakeAway: true,
-    canEatIn: true,
-  },
-  {
-    name: "Billy Brick",
-    tags: [],
-    canTakeAway: true,
-    canEatIn: false,
-  },
-  {
-    name: "Shin Jung",
-    tags: ["asiatique"],
-    canTakeAway: true,
-    canEatIn: true,
-  },
-  {
-    name: "Mme Shawn",
-    tags: ["asiatique"],
-    canTakeAway: true,
-    canEatIn: true,
-  },
-  {
-    name: "Pokeshop",
-    tags: ["asiatique"],
-    canEatIn: false,
-    canTakeAway: true,
-  },
-  {
-    name: "Parisik",
-    tags: ["asiatique"],
-    canEatIn: true,
-    canTakeAway: true,
-  },
-  {
-    name: "Augustin",
-    tags: [],
-    canEatIn: true,
-    canTakeAway: false,
-  },
-];
+  return arrayWithRandomRestaurant[0];
+};
