f7d892df181311f5e9cf4692aad74e2c44ad21aa
Loïc Chau <loicch@theodo.fr>
2023-12-12 23:26:18 +0100

    feat: add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import Link from "next/link";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const raleway = Raleway({ subsets: ["latin"] });
 
@@ -33,6 +34,7 @@ export default function RootLayout({
           </nav>
         </div>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
