de7849d190d8b10c13a8b058e60e814e2aeff26e
corenting <corenting@theodo.fr>
2024-05-10 09:45:15 +0200

    Add maps URL parser to retrieve restaurant position

--- src/core/restaurants/interface.ts
+++ src/core/restaurants/interface.ts
@@ -1,22 +1,25 @@
 export interface RestaurantInfos {
-    name: string;
-    canEatIn: boolean;
-    canTakeAway: boolean;
-    vegetarianFriendly: boolean;
-    meatLover: boolean;
-    mapUrl: string|null;
-    lessThanTenEuros: boolean;
-  }
+  name: string;
+  canEatIn: boolean;
+  canTakeAway: boolean;
+  vegetarianFriendly: boolean;
+  meatLover: boolean;
+  mapUrl: string | null;
+  lessThanTenEuros: boolean;
+  restaurantPosition: [number, number];
+}
 
 export type RestaurantBooleanKeys = keyof {
-    [Key in keyof RestaurantInfos as RestaurantInfos[Key] extends boolean? Key : never]: never;
-}
+  [Key in keyof RestaurantInfos as RestaurantInfos[Key] extends boolean
+    ? Key
+    : never]: never;
+};
 
 const objectBooleanKeys: Record<RestaurantBooleanKeys, undefined> = {
-  canEatIn : undefined,
-  canTakeAway : undefined,
-  vegetarianFriendly : undefined, 
-  meatLover : undefined,
-  lessThanTenEuros : undefined
-}
-export const restaurantBooleanKeys = Object.keys(objectBooleanKeys)
+  canEatIn: undefined,
+  canTakeAway: undefined,
+  vegetarianFriendly: undefined,
+  meatLover: undefined,
+  lessThanTenEuros: undefined,
+};
+export const restaurantBooleanKeys = Object.keys(objectBooleanKeys);
