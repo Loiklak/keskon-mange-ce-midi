582810fb828203041106453243edc1c5bed4796e
corenting <corenting@theodo.fr>
2024-05-10 10:29:07 +0200

    Add the 2 positions on the map card and zoom on the positions

--- src/components/MapContainerComponent/MapContainerComponent.tsx
+++ src/components/MapContainerComponent/MapContainerComponent.tsx
@@ -1,28 +1,34 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { LatLngExpression } from "leaflet";
+import { LatLngExpression, latLngBounds } from "leaflet";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css"; // Re-uses images from ~leaflet package
 import "leaflet-defaulticon-compatibility";
 
-const MapContainerComponent = () => {
-  const position: LatLngExpression = [51.505, -0.09];
+interface Props {
+  name: string;
+  positionRestaurant: [number, number];
+}
+
+const MapContainerComponent = ({ name, positionRestaurant }: Props) => {
+  const positionTheodo: LatLngExpression = [48.882737, 2.322391];
+  const bounds = latLngBounds([positionRestaurant, positionTheodo]);
 
   return (
     <MapContainer
-      center={position}
-      zoom={13}
       scrollWheelZoom={false}
-      style={{ height: "100%", width: "100%" }}
+      bounds={bounds}
+      style={{ height: "100%", width: "100%", borderRadius: "10%" }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
+      <Marker position={positionTheodo}>
+        <Popup>Theodo</Popup>
+      </Marker>
+      <Marker position={positionRestaurant}>
+        <Popup>{name}</Popup>
       </Marker>
     </MapContainer>
   );
