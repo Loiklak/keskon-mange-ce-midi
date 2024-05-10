7b88b4b2823cff53338124174be092464255f738
corenting <corenting@theodo.fr>
2024-05-10 14:53:27 +0200

    Add distance and time estimation to the map

--- src/components/RestaurantInfosCard/RestaurantInfosCard.tsx
+++ src/components/RestaurantInfosCard/RestaurantInfosCard.tsx
@@ -1,24 +1,35 @@
 import { RestaurantInfos } from "@/core/restaurants/interface";
 import MapContainerComponent from "../MapContainerComponent/MapContainerComponent";
 import styles from "./RestaurantInfosCard.module.css";
+import { calculateDistance, positionTheodo, walkingSpeed } from "@/lib/utils";
 
 interface Props {
   restaurant?: RestaurantInfos;
 }
 
 export const RestaurantInfosCard = ({ restaurant }: Props) => {
+  if (!restaurant) {
+    return null;
+  }
+  if (!restaurant.restaurantPosition) {
+    return null;
+  }
+  const distanceRestaurantTheodoKM: number = calculateDistance(
+    positionTheodo,
+    restaurant.restaurantPosition
+  );
+  const distanceRestaurantTheodoM = distanceRestaurantTheodoKM * 1000;
+  const timeRestaurantTheodo = (60 * distanceRestaurantTheodoKM) / walkingSpeed;
   return (
     <div>
-      <p>{restaurant ? restaurant.name : "Je réfléchis"}</p>
+      <p>
+        {`${restaurant.name} : ${Math.round(timeRestaurantTheodo)} min (${Math.round(distanceRestaurantTheodoM)} m)`}
+      </p>
       <div className={styles.mapZone}>
-        {restaurant ? (
-          <MapContainerComponent
-            name={restaurant.name}
-            positionRestaurant={restaurant.restaurantPosition}
-          ></MapContainerComponent>
-        ) : (
-          <div>Je réfléchis</div>
-        )}
+        <MapContainerComponent
+          name={restaurant.name}
+          positionRestaurant={restaurant.restaurantPosition}
+        ></MapContainerComponent>
       </div>
     </div>
   );
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,24 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const positionTheodo: [number, number] = [48.882737, 2.322391];
+
+export const calculateDistance = (
+  position1: [number, number],
+  position2: [number, number]
+) => {
+  const [lat1, lon1] = position1;
+  const [lat2, lon2] = position2;
+  const R = 6371; // Radius of the earth in km
+  const dLat = deg2rad(lat2 - lat1); // deg2rad below
+  const dLon = deg2rad(lon2 - lon1);
+  const d = R * (Math.abs(dLat) + Math.abs(dLon)); // Norm 1 distance (mostly perpendicular streets)
+  return d;
+};
+
+export const walkingSpeed = 5; // km/h
+
+const deg2rad = (deg: number) => {
+  return deg * (Math.PI / 180);
+};
