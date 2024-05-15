98e26e4c9d6fb47e6c18df314260dd73b5a0b077
corenting <corenting@theodo.fr>
2024-05-15 14:13:56 +0200

    Add an error message if Google Maps URL is wrong

--- src/components/MapInfosComponent/MapInfosComponent.tsx
+++ src/components/MapInfosComponent/MapInfosComponent.tsx
@@ -2,6 +2,7 @@ import { RestaurantInfos } from "@/core/restaurants/interface";
 import MapContainerComponent from "../MapContainerComponent/MapContainerComponent";
 import styles from "./MapInfosComponent.module.css";
 import { calculateDistance, positionTheodo, walkingSpeed } from "@/lib/utils";
+import Link from "next/link";
 
 interface Props {
   restaurant?: RestaurantInfos;
@@ -12,7 +13,23 @@ export const MapInfosComponent = ({ restaurant }: Props) => {
     return null;
   }
   if (!restaurant.restaurantPosition) {
-    return null;
+    return (
+      <div className={styles.mapZone}>
+        <p>{`Oups, il nous manque des infos sur ce resto`}</p>
+        <p>{`----------`}</p>
+        <p>
+          {`Vous pouvez améliorer ce site en complétant les infos du resto (URL Google Maps) sur le lien ci-dessous`}
+        </p>
+        <p>{`👇👇👇👇👇👇`}</p>
+        <a
+          href={`https://docs.google.com/spreadsheets/d/1B1kD-kZQiJUEYcSmAXom_lY7_JhOLp9UTq0Uf44wNY0/edit#gid=0`} //document id = variable d'environnement
+          target="_blank"
+          className={styles.lien}
+        >
+          Google Sheets
+        </a>
+      </div>
+    );
   }
   const distanceRestaurantTheodoKM: number = calculateDistance(
     positionTheodo,
