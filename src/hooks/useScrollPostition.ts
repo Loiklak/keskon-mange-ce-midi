815f611913b9a487657fd9773ca44cbdfe820f9b
cyprienraf <cyprienr@theodo.fr>
2024-05-17 12:23:35 +0200

    style: remove comments

--- src/hooks/useScrollPostition.ts
+++ src/hooks/useScrollPostition.ts
@@ -8,18 +8,15 @@ function useScrollPosition() {
 
   useEffect(() => {
     const handleScroll = () => {
-      // Update scrollY state with the current scroll position
       setScroll({ x: window.scrollX, y: window.scrollY });
     };
 
-    // Add event listener to the scroll event
     window.addEventListener("scroll", handleScroll);
 
-    // Clean up by removing the event listener when the component unmounts
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []); // Empty dependency array ensures that this effect runs only once
+  }, []);
 
   return scroll;
 }
