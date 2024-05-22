311ac315f6842ed1dbd990d9e937ddb0c5df9427
cyprienraf <cyprienr@theodo.fr>
2024-05-22 15:39:59 +0200

    feat: add a function displaying one review

--- src/components/RestaurantCard/Review/ReviewComponent.tsx
+++ src/components/RestaurantCard/Review/ReviewComponent.tsx
@@ -1,3 +1,7 @@
+import {
+  ReviewInfos,
+  convertEvaluationToStringToDisplay,
+} from "@/core/avis/type/interface";
 import styles from "./ReviewComponent.module.css";
 
 interface ReviewComponentProps {
@@ -6,6 +10,37 @@ interface ReviewComponentProps {
 
 export const ReviewComponent: React.FC<ReviewComponentProps> = ({ review }) => {
   return review ? <div>comment</div> : displayNoReview();
+};
+
+export const displayOneReviewComment = (review: ReviewInfos) => {
+  return (
+    <div className={styles.review} id="slider">
+      <p className={styles.evaluation}>
+        {convertEvaluationToStringToDisplay(review.evaluation)}
+      </p>
+      <div className={styles.comment}>
+        <p>❝ {review.comment} ❞</p>
+      </div>
+      <div className={styles.containerCommentAndAuthor}>
+        <div>
+          <p className={styles.addComment}>
+            <a
+              href="https://docs.google.com/spreadsheets/d/1B1kD-kZQiJUEYcSmAXom_lY7_JhOLp9UTq0Uf44wNY0/edit#gid=915087104"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Ajouter un commentaire !
+            </a>
+          </p>
+        </div>
+        <div>
+          <p className={styles.author}>
+            -{review.author ? review.author : "Anonyme"}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 export const displayNoReview = () => {
   return (
