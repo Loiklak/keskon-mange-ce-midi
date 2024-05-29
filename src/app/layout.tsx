5e9d139fbbb4b663ddb2fa643a718d834a3032a0
cyprienraf <cyprienr@theodo.fr>
2024-05-29 10:38:16 +0200

    feat: use react query client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import { Raleway } from "next/font/google";
 import Link from "next/link";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { ReactQueryClientProvider } from "@/components/ReactQueryClientProvider/ReactQueryClientProvider";
+
+
 
 const raleway = Raleway({ subsets: ["latin"] });
 
@@ -21,6 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    <ReactQueryClientProvider>
     <html lang="en" className="h-full">
       <body
         className={`h-full bg-slate-100 overflow-auto ${raleway.className}`}
@@ -47,5 +51,6 @@ export default function RootLayout({
         <SpeedInsights />
       </body>
     </html>
+    </ReactQueryClientProvider>
   );
 }
